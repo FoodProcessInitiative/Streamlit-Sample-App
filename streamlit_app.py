6824a3548ca4846648430894cb39c5ffdbdc6668
FoodProcessInitiative <167958039+FoodProcessInitiative@users.noreply.github.com>
2025-06-23 23:07:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,19 @@
 import streamlit as st
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
-import pandas as pd
-if uploaded_file is not None:
-    #df = pd.read_csv(path, sep=",", encoding = 'utf-8') 
-    df = pd.read_csv(
-        uploaded_file,
-        #sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
-        sep=",",
-        engine='python',
-        na_values='-',
-        header=None)
 
-st.markdown('### アクセスログ（先頭5件）')
-st.write(df.head(5))
+name = st.text_input("名前を入力してください")
+st.write("こんにちは", name)
+
+#uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+#import pandas as pd
+#if uploaded_file is not None:
+#    #df = pd.read_csv(path, sep=",", encoding = 'utf-8') 
+#    df = pd.read_csv(
+#        uploaded_file,
+#        #sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+#        sep=",",
+#        engine='python',
+#        na_values='-',
+#        header=None)
+#
+#st.markdown('### アクセスログ（先頭5件）')
+#st.write(df.head(5))
